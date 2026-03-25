ea3225d3d71963ded08383449144ac9235404cc0
NateC-F <148012685+NateC-F@users.noreply.github.com>
2026-03-25 15:20:43 -0400

    Updating SQL Script

--- src/main/java/com/example/seniorfinal/Controllers/CheckoutController.java
+++ src/main/java/com/example/seniorfinal/Controllers/CheckoutController.java
@@ -1,4 +1,12 @@
 package com.example.seniorfinal.Controllers;
 
+import com.example.seniorfinal.Utilities.SceneID;
+import com.example.seniorfinal.Utilities.SceneManager;
+
 public class CheckoutController {
+    public void goBack()
+    {
+        SceneManager.switchTo(SceneID.ListingViewScreen);
+    }
+
 }
--- src/main/java/com/example/seniorfinal/Controllers/OrderHistoryController.java
+++ src/main/java/com/example/seniorfinal/Controllers/OrderHistoryController.java
@@ -1,5 +1,13 @@
 package com.example.seniorfinal.Controllers;
 
+import com.example.seniorfinal.Utilities.SceneID;
+import com.example.seniorfinal.Utilities.SceneManager;
+
 public class OrderHistoryController
 {
+    public void goBack()
+    {
+        SceneManager.switchTo(SceneID.ProfileScreen);
+    }
+
 }
